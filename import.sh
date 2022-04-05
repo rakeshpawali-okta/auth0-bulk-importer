a529b3a19bb25229909dee7dfe37589270a348c6
Amin Abbaspour <a.abbaspour@gmail.com>
2022-04-05 12:35:26 +1000

    input from snsw (#1)

--- import.sh
+++ import.sh
@@ -5,13 +5,16 @@ set -eo pipefail
 declare input_folder=''
 export output_folder=''
 export connection_id=''
+export upsert=false
+export send_completion_email=false
+export summary_file='import-summary.csv'
 
 declare -i job_max=1
 
 command -v awk >/dev/null       || { echo >&2 "ERROR: awk not found."; exit 3; }
 command -v jq >/dev/null        || { echo >&2 "ERROR: jq not found."; exit 3; }
 command -v base64 >/dev/null    || { echo >&2 "ERROR: base64 not found."; exit 3; }
-command -v parallel >/dev/null  || { echo >&2 "ERROR: base64 not found."; exit 3; }
+command -v parallel >/dev/null  || { echo >&2 "ERROR: parallel not found."; exit 3; }
 
 export retries=100
 
@@ -23,7 +26,11 @@ USAGE: $0 [-e env] [-a access_token] [-c connection_id] [-i input-folder] [-o ou
         -c id       # connection_id
         -j count    # parallel job count. defaults to ${job_max}
         -i folder   # input folder containing import JSON files
-        -o folder   # out folder to move imported files
+        -o folder   # out folder to move imported files. default is same as input
+        -s file     # change summary file name. default is ${summary_file}
+        -r count    # retry count on HTTP and rate-limit errors with exponential backoff. default in ${retries}
+        -u          # run in upsert mode. default is false
+        -S          # send completion email. default is false
         -h|?        # usage
         -v          # verbose
 
@@ -33,7 +40,7 @@ END
     exit $1
 }
 
-while getopts "e:a:i:o:c:j:hv?" opt
+while getopts "e:a:i:o:c:j:s:r:uShv?" opt
 do
     case ${opt} in
         e) source "${OPTARG}";;
@@ -42,6 +49,10 @@ do
         j) job_max=${OPTARG};;
         i) input_folder=${OPTARG};;
         o) output_folder=${OPTARG};;
+        s) summary_file=${OPTARG};;
+        r) retries=${OPTARG};;
+        u) upsert=true;;
+        S) send_completion_email=true;;
         v) set -x;;
         h|?) usage 0;;
         *) usage 1;;
@@ -51,39 +62,55 @@ done
 [[ -z "${access_token}" ]] && { echo >&2 "ERROR: access_token undefined. export access_token='PASTE' "; usage 1; }
 [[ -z "${connection_id}" ]] && { echo >&2 "ERROR: connection_id undefined."; usage 1; }
 [[ -z "${input_folder}" ]] && { echo >&2 "ERROR: input_folder undefined."; usage 1; }
-[[ -z "${output_folder}" ]] && { echo >&2 "ERROR: output_folder undefined."; usage 1; }
+
+[[ -z "${output_folder}" ]] && output_folder="${input_folder}"
 
 export AUTH0_DOMAIN_URL=$(echo "${access_token}" | awk -F. '{print $2}' | base64 -di 2>/dev/null | jq -r '.iss')
 
 function upload() {
     local input_file=$(readlink -m "${1}")
 
+    echo -n "Processing: ${input_file} => "
     local job_id=$(curl -s -H "Authorization: Bearer ${access_token}" \
       -F users=@"${input_file}" \
       -F connection_id="${connection_id}" \
-      -F upsert=false \
-      -F send_completion_email=false \
+      -F upsert=${upsert} \
+      -F send_completion_email=${send_completion_email} \
       --retry ${retries} \
       --url "${AUTH0_DOMAIN_URL}api/v2/jobs/users-imports" | jq -r '.id')
 
-
-    echo "${job_id}"
+    local submitted_at=$(date +%FT%T)
+    echo -n " ${job_id} => "
     local status='pending'
+    if [[ "${job_id}" == "null" ]]; then
+      status='failed'
+    fi
 
+    local payload=''
     while [[ "${status}" == "pending" ]] ; do
-      status=$(curl -s -H "Authorization: Bearer ${access_token}" \
-      --url ${AUTH0_DOMAIN_URL}api/v2/jobs/${job_id} | jq -r '.status')
+      payload=$(curl -s -H "Authorization: Bearer ${access_token}" \
+                          --url "${AUTH0_DOMAIN_URL}api/v2/jobs/${job_id}")
+      status=$(echo "${payload}" | jq -r '.status')
       sleep 1
     done
+
+    local finished_at=$(date +%FT%T)
 
     local output_file=$(readlink -m "${output_folder}/$(basename "${input_file}")-${status}-${job_id}")
 
     echo "${output_file}"
     mv "${input_file}" "${output_file}"
+
+    printf "%s,%s,%s,%s,%s,%s\n" $(basename "${input_file}") "${job_id}" "${submitted_at}" "${finished_at}" "${status}" \
+      $(echo "${payload}" | jq -r '.summary | "\(.total),\(.inserted),\(.updated),\(.failed)"') >> "${summary_file}"
+
 }
 
 export -f upload
 export access_token
 
-find "${input_folder}" -name '*.json' -type f -print0 | \
+echo "# import job started at: $(date)" > "${summary_file}"
+echo "file,job_id,submitted_at,finished_at,status,total,inserted,updated,failed" >> "${summary_file}"
+
+find "${input_folder}" -name '*.json' -type f -print0 | sort -z | \
   parallel -0 -j${job_max} upload {}
--- readme.md
+++ readme.md
@@ -1,5 +1,68 @@
 # Auth0 Bulk Importer
-This is a single script that uploads JSON files as import jobs to Auth0 and 
+This is a single command line that uploads JSON files as import jobs to Auth0 and 
 moves them to target folder was job is completed.
 
 
+## Requirement and Dependencies
+Following command line tools should be installed:
+
+* awk
+* jq
+* base64
+* parallel
+
+
+### Installing in Mac
+
+```bash
+brew inatall awk jq base64 parallel
+```
+
+
+## Execution
+
+### Management API Token
+You'll need to obtain a management API access token and export it shell to be able to call APIv2 [jobs](https://auth0.com/docs/api/management/v2#!/Jobs/get_jobs_by_id) API. 
+Minimum scopes required are: 
+* `create:users`
+* `read:users`
+
+> Note: Since execution of all jobs would take hours for large jobs, we recommend a long-lived access_token, enough to last for full duration of import.
+
+```bash
+export access_token='APIv2_ACCESS_TOKEN'
+```
+
+### Database Connection
+Determine `connection_id` of the target database connection. 
+
+> Note: Make sure M2M client for APIv2 is enabled against target database connection.
+
+### Command Line
+```bash
+USAGE: ./import.sh [-e env] [-a access_token] [-c connection_id] [-i input-folder] [-o output-folder] [-v|-h]
+        -e file     # .env file location (default cwd)
+        -a token    # access_token. default from environment variable
+        -c id       # connection_id
+        -j count    # parallel job count. defaults to 1
+        -i folder   # input folder containing import JSON files
+        -o folder   # out folder to move imported files. default is same as input
+        -s file     # change summary file name. default is import-summary.csv
+        -r count    # retry count on HTTP and rate-limit errors with exponential backoff. default in 100
+        -u          # run in upsert mode. default is false
+        -S          # send completion email. default is false
+        -h|?        # usage
+        -v          # verbose
+
+eg,
+     ./import.sh -c con_Z1QogOOq4sGa1iR9 -i users -o result
+```
+
+### Parallel Job Count
+This number is 2 for public cloud. For private cloud it can be increased to up to 20. Check your TAM/CSM. 
+
+### Monitoring and Detailed Error
+```bash
+tail -f import-summary.csv
+./job-status.sh -i JOB-ID -d
+```
