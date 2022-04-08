7250b488857971acc76aa27421ef9e9623d199fb
Amin Abbaspour <a.abbaspour@gmail.com>
2022-04-08 14:28:36 +1000

    handle null job-id (when over capacity) added a report generator

--- import.sh
+++ import.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 
 declare input_folder=''
 export output_folder=''
+export report_folder=''
 export connection_id=''
 export upsert=false
 export send_completion_email=false
@@ -26,9 +27,10 @@ USAGE: $0 [-e env] [-a access_token] [-c connection_id] [-i input-folder] [-o ou
         -c id       # connection_id
         -j count    # parallel job count. defaults to ${job_max}
         -i folder   # input folder containing import JSON files
-        -o folder   # out folder to move imported files. default is same as input
+        -o folder   # output folder to move imported files. default is same as input
+        -r folder   # report folder to move report files. default is same as input
         -s file     # change summary file name. default is ${summary_file}
-        -r count    # retry count on HTTP and rate-limit errors with exponential backoff. default in ${retries}
+        -n count    # number of retries on HTTP and rate-limit errors with exponential backoff. default in ${retries}
         -u          # run in upsert mode. default is false
         -S          # send completion email. default is false
         -h|?        # usage
@@ -40,7 +42,7 @@ END
     exit $1
 }
 
-while getopts "e:a:i:o:c:j:s:r:uShv?" opt
+while getopts "e:a:i:o:c:j:s:r:n:uShv?" opt
 do
     case ${opt} in
         e) source "${OPTARG}";;
@@ -49,8 +51,9 @@ do
         j) job_max=${OPTARG};;
         i) input_folder=${OPTARG};;
         o) output_folder=${OPTARG};;
+        r) report_folder=${OPTARG};;
         s) summary_file=${OPTARG};;
-        r) retries=${OPTARG};;
+        n) retries=${OPTARG};;
         u) upsert=true;;
         S) send_completion_email=true;;
         v) set -x;;
@@ -59,30 +62,42 @@ do
     esac
 done
 
+[[ -z "${output_folder}" ]] && output_folder="${input_folder}"
+[[ -z "${report_folder}" ]] && report_folder="${input_folder}"
+
 [[ -z "${access_token}" ]] && { echo >&2 "ERROR: access_token undefined. export access_token='PASTE' "; usage 1; }
 [[ -z "${connection_id}" ]] && { echo >&2 "ERROR: connection_id undefined."; usage 1; }
 [[ -z "${input_folder}" ]] && { echo >&2 "ERROR: input_folder undefined."; usage 1; }
-[[ -z "${output_folder}" ]] && output_folder="${input_folder}"
 
 [[ ! -d "${input_folder}" ]] && { echo >&2 "ERROR: input is not a folder: ${input_folder}"; usage 1; }
+
+mkdir -p "${output_folder}"
+mkdir -p "${report_folder}"
+
 [[ ! -d "${output_folder}" ]] && { echo >&2 "ERROR: output is not a folder: ${output_folder}"; usage 1; }
+[[ ! -d "${report_folder}" ]] && { echo >&2 "ERROR: report is not a folder: ${report_folder}"; usage 1; }
 
 export AUTH0_DOMAIN_URL=$(echo "${access_token}" | awk -F. '{print $2}' | base64 -di 2>/dev/null | jq -r '.iss')
 
 function upload() {
     local input_file=$(readlink -m "${1}")
 
-    echo -n "Processing: ${input_file} => "
+    echo -n $(basename "${input_file}")
     local job_id=$(curl -s -H "Authorization: Bearer ${access_token}" \
       -F users=@"${input_file}" \
       -F connection_id="${connection_id}" \
       -F upsert=${upsert} \
       -F send_completion_email=${send_completion_email} \
-      --retry ${retries} \
+      --retry "${retries}" \
       --url "${AUTH0_DOMAIN_URL}api/v2/jobs/users-imports" | jq -r '.id')
 
+    if [[ "${job_id}" == null* ]]; then
+      echo " error. skipping"
+      return
+    fi
+
     local submitted_at=$(date +%FT%T)
-    echo -n " ${job_id} => "
+    echo -n " (${job_id}) => "
     local status='pending'
     if [[ "${job_id}" == "null" ]]; then
       status='failed'
@@ -100,8 +115,9 @@ function upload() {
 
     local output_file=$(readlink -m "${output_folder}/$(basename "${input_file}")") # -${status}-${job_id}
 
-    echo "${output_file}"
+    echo $(basename "${output_file}")
     mv "${input_file}" "${output_file}"
+    echo "${payload}" | jq . > "${report_folder}/${job_id}.json"
 
     printf "%s,%s,%s,%s,%s,%s\n" $(basename "${input_file}") "${job_id}" "${submitted_at}" "${finished_at}" "${status}" \
       $(echo "${payload}" | jq -r '.summary | "\(.total),\(.inserted),\(.updated),\(.failed)"') >> "${summary_file}"
@@ -111,8 +127,10 @@ function upload() {
 export -f upload
 export access_token
 
-echo "# import job started at: $(date)" > "${summary_file}"
+echo "# started: $(date +%FT%T)" > "${summary_file}"
 echo "file,job_id,submitted_at,finished_at,status,total,inserted,updated,failed" >> "${summary_file}"
 
-find "${input_folder}" -name '*.json' -type f -print0 | sort -z | \
-  parallel -0 -j${job_max} upload {}
+find "${input_folder}" -name '*.json' -type f -print0 | sort -zn | \
+  parallel -0 -j "${job_max}" upload {}
+
+echo "# ended: $(date +%FT%T)" >> "${summary_file}"
---
+++ report.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+declare summary='import-summary.csv'
+
+function usage() {
+    cat <<END >&2
+USAGE: $0 [-f summary-file]
+        -f file     # export report from summary file. default is ${summary}
+        -h|?        # usage
+        -v          # verbose
+
+eg,
+     $0 -f users-import-summary.csv
+END
+    exit $1
+}
+
+while getopts "f:hv?" opt
+do
+    case ${opt} in
+        f) summary=${OPTARG};;
+        v) set -x;;
+        h|?) usage 0;;
+        *) usage 1;;
+    esac
+done
+
+[[ -z "${summary}" ]] && { echo >&2 "ERROR: summary file undefined."; usage 1; }
+[[ ! -f "${summary}" ]] && { echo >&2 "ERROR: input is not a file: ${summary}"; usage 1; }
+
+readonly start_date=$(head -n1 "${summary}" | awk '{print $NF}')
+readonly end_date=$(tail -n1 "${summary}" | awk '{print $NF}')
+
+readonly duration=$(echo "`date -d ${end_date} +%s` - `date -d ${start_date} +%s`" | bc)
+
+echo "Start date: ${start_date}"
+echo "End   date: ${end_date}"
+echo "Duration  : ${duration} seconds"
+
+echo -n "Total     : "
+awk -F, '(NR>2){print $6}' import-summary.csv | head -n -1  | paste -sd+ | bc
+
+echo -n "Inserted  : "
+awk -F, '(NR>2){print $7}' import-summary.csv | head -n -1  | paste -sd+ | bc
+
+echo -n "Updated   : "
+awk -F, '(NR>2){print $8}' import-summary.csv | head -n -1  | paste -sd+ | bc
+
+echo -n "Failed    : "
+awk -F, '(NR>2){print $9}' import-summary.csv | head -n -1  | paste -sd+ | bc
